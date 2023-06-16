e1a6581638947f1b7d04d5354ddd7efedee954fa
vuvohoang1823 <vuvohoang1823@gmail.com>
2023-06-16 23:32:10 +0700

    ex-7

---
+++ src/Components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <div className='footer'>copyright © 2022</div>
+  )
+}
+
+export default footer
---
+++ src/Share/ListOfPlayer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ListOfPlayer =[
+    {id: 1, name: 'Cristiano Ronaldo', club: 'Manchester United', img: '/asset/images/cr.jpg'},
+    {id: 2, name: 'Kante', club: 'Chelsea', img:'/asset/images/kante.jpg'},
+    {id: 3, name: 'Messi', club: 'PSG', img:'/asset/images/messi.jpg'},
+    {id: 4, name: 'Neymar', club: 'PSG',img:'/asset/images/neymar.jpg'},
+    {id: 5, name: 'Kane', club: 'Tottemham',img:'/asset/images/kane.jpg'},
+    {id: 6, name: 'Haaland', club: 'Manchester City', img:'/asset/images/haaland.jpg'}
+  ];
+
+export default ListOfPlayer
